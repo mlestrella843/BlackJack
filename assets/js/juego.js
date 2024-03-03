28f1f98474a34714a44fb2f0d4f5ae59aeabdab9
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-03-03 06:40:37 -0700

    Funcion que pide una carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = [ 'A', 'J', 'Q', 'K' ]; 
 
+//Esta funcion crea las barajas
 const crearDeck = () => {
     for ( let i=2; i<=10; i++ ) {
         for( const tipo of tipos) {
@@ -21,12 +22,26 @@ const crearDeck = () => {
         }
     }
 
+    // console.log(deck);
+    deck = _.shuffle(deck); // Esta funcion de la biblioteca underscore, mezcla el array de cartas
     console.log(deck);
-
-    deck = _.shuffle(deck); // mezcla el array de cartas
     return deck
-    console.log(deck);
-
+   
 }
 
 crearDeck();
+//Funcion que tomara una carta.
+
+const pedirCarta = () => {
+
+    if(deck.length === 0){
+        throw 'No hay cartas en el deck';
+    }
+
+    let carta = deck.pop();
+    console.log(deck);
+    console.log({carta});
+    return carta;
+}
+
+pedirCarta();
